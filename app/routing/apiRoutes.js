ba16e29158d2d0726c1780304eaa21b06b89955e
Dano <danielaherniv@gmail.com>
2019-09-29 23:18:06 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,22 @@
-let amigo = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function (app) {
-
     app.get("/api/friends", function (req, res) {
-        res.json(amigo);
-
+        res.json(friends);
     });
 
     app.post("/api/friends", function (req, res) {
+        var totalDifference = 0;
 
-        let totalDifference = 0;
-        let bestMatch = {
+        var bestMatch = {
             name: "",
             photo: "",
             friendDifference: 1000
         };
 
-        let userDate = req.body;
-        let userName = userData.name;
-        let userScores = userData.scores;
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
 
         var b = userScores.map(function (item) {
             return parseInt(item, 10);
@@ -27,61 +25,40 @@ module.exports = function (app) {
             name: req.body.name,
             photo: req.body.photo,
             scores: b
-        }
+        };
 
         console.log("Name: " + userName);
-        console.log("User score " + userScores);
+        console.log("User Score " + userScores);
 
-        let sum = b.reduce((a, b) => a + b, 0);
+        var sum = b.reduce((a, b) => a + b, 0);
+
         console.log("Sum of users score " + sum);
-        console.log("Best match friend diff" + bestMatch.friendDifference);
-        console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        console.log("Best match friend diff " + bestMatch.friendDifference);
+        console.log("+++++++=================++++++++++");
 
-        for (let i = 0; i < amigo.length; index++) {
-            console.log(amigo[i].name);
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
             totalDifference = 0;
             console.log("Total Diff " + totalDifference);
             console.log("Best match friend diff " + bestMatch.friendDifference);
 
-            let bfriendScore = amigo[i].scores.reduce((a, b) => a + b, 0);
+            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
             console.log("Total friend score " + bfriendScore);
             totalDifference += Math.abs(sum - bfriendScore);
-            console.log("---------------------------> " + totalDifference);
+            console.log("-------------------------> " + totalDifference);
 
             if (totalDifference <= bestMatch.friendDifference) {
                 bestMatch.name = friends[i].name;
                 bestMatch.photo = friends[i].photo;
                 bestMatch.friendDifference = totalDifference;
             }
-
-            console.log(totalDifference + "Total Difference");
+            console.log(totalDifference + " Total Difference");
         }
-
         console.log(bestMatch);
+
         friends.push(userData);
-        console.log("New User Added");
+        console.log("New user added");
         console.log(userData);
         res.json(bestMatch);
-
-
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+};
--- server.js
+++ server.js
@@ -1,35 +1,22 @@
- // Dependencies
- // =============================================================
- var express = require("express");
- var bodyParser = require("body-parser");
- var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
- // Sets up the Express App
- // =============================================================
- var app = express();
- var PORT = process.env.PORT || 3000;
+var app = express();
+var port = process.env.PORT || 3000;
 
- // Sets up the Express app to handle data parsing
- app.use(express.urlencoded({
-     extended: true
- }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
 
- app.use(express.json());
+app.use(express.static("app/public"));
 
- // parse application/text
- app.use(bodyParser.text())
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
- // parse some custom thing into a Buffer
- app.use(bodyParser.raw({
-     type: 'application/vnd.api+json'
- }))
-
- app.use(express.static("app/public"));
-
- require("./app/routing/apiRoutes")(app);
- require("./app/routing/htmlRoutes")(app);
-
-
- app.listen(PORT, function () {
-     console.log("App listening on PORT: " + PORT);
- });
+app.listen(port, () => console.log("Listening on port %s", port));
