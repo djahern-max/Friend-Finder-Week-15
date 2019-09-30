042c53b23679d10e7dfbfae68a2aa879bc277187
Dane Ahern <danielaherniv@gmail.com>
2019-09-30 13:29:57 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 var friends = [{
-        name: "Tom Hardy",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Jamie Dornan",
-        photo: "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
-        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
-    },
-    {
-        name: "Anthony Joshua",
-        photo: "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
-        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
-    },
-    {
-        name: "Henry Cavill",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
-        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
-    },
-    {
-        name: "Emily Ratajowski",
-        photo: "https://pmchollywoodlife.files.wordpress.com/2014/10/Emily-Ratajkowski-Reveals-Rock-Hard-Abs-In-Teeny-Crop-Top-While-Vacationing-With-Husband-gallery.jpg",
-        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
-    },
-    {
-        name: "Blake Lively",
-        photo: "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
-        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
-    },
-    {
-        name: "Kate Upton",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
-    }
+    name: "Danny Devito",
+    photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    name: "Number 2",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/No_2_APTSWSM.jpg/200px-No_2_APTSWSM.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    name: "The One",
+    photo: "https://s-media-cache-ak0.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    name: "Trinity",
+    photo: "http://vignette3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    name: "Dr. Evil",
+    photo: "https://at-cdn-s01.audiotool.com/2012/05/28/documents/ei368KjdlP5qy6gILDLtH9cEAf6H/0/cover256x256-b31436c2a9bc4645b1ad67bc09705cd7.jpg",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+    name: "Mia Wallace",
+    photo: "https://s-media-cache-ak0.pinimg.com/originals/a9/01/29/a9012982eea651ee31e21f4ad11fe96b.jpg",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+},
+{
+    name: "Princess Leia",
+    photo: "http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
 ];
 module.exports = friends;
